374b1ef35be2e3678e3b74570039ff1cbda90c36
hirotori <hirotori620@gmail.com>
2025-01-21 15:59:52 +0900

    add test for searching ref_cell

---
+++ test/test_refcell_search_parallel.f90
@@ -0,0 +1,57 @@
+program test
+    use base_importer_m, only: ugrid_struct_t, delete_ugrid
+    use vtk_importer_m
+    use flow_field_m
+    use omp_lib
+    implicit none
+    integer,parameter :: nsample = 5
+    integer :: locations(nsample) = [1, 12, 4, 7, 5]
+    integer :: solutions(nsample)
+    real(8) pts(3,nsample), rn_(3)
+    integer i, cellId
+    real(8),allocatable :: v(:,:)
+    type(vtk_importer_t) vtk_importer
+    type(ugrid_struct_t) vtk_ugrid
+
+    call omp_set_num_threads(4)
+
+    block
+        integer nseed
+        integer,allocatable :: seeds(:)
+        call random_seed(size=nseed)
+        allocate(seeds(nseed), source=100)
+        call random_seed(put=seeds)
+    end block
+
+    call vtk_importer%open_ascii_on()
+    call vtk_importer%open_stream_on()
+    call vtk_importer%open_file("sample_small.vtk")
+    call vtk_importer%read_file(vtk_ugrid, shift_index=.true.)
+    call vtk_importer%close()
+
+    allocate(v(3,vtk_ugrid%ncell), source=0.d0)
+    vtk_ugrid%cell_velocity = v
+    call construct_flow_field(vtk_ugrid, CELL_TYPE_VTK, FACE_VERT_DEF_VTK)
+    call delete_ugrid(vtk_ugrid)
+
+    do i = 1, nsample
+        call random_number(rn_)
+        pts(:,i) = mv_flow_field%cell_centers(:,locations(i)) !+ rn_*0.1d0
+    end do
+    
+    !$omp parallel do private(cellId)
+    do i = 1, nsample
+        cellId = 1
+        call search_reference_cell(pts(:,i), pts(:,i), cellId, 10)
+        solutions(i) = cellId
+    end do
+    
+    print "(*(i0,1x))", locations
+    print "(*(i0,1x))", solutions
+
+    do i = 1, nsample
+        if ( locations(i) /= solutions(i) ) then
+            error stop
+        end if
+    end do
+end program test
